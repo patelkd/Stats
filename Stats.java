ba513dc3d441221a7e918ac3754682755a878b0c
patelkd <karishma1126@yahoo.com>
2013-09-24 08:51:24 -0600

    Update Stats.java
    changed method names

--- Stats.java
+++ Stats.java
@@ -1,6 +1,6 @@
 public class Stats {
 	public static void main(String[] args) {
-		
+
 		//Max
 		//Min
 		//Mean
@@ -11,34 +11,34 @@ public class Stats {
 		//standard deviation
 
 
-		int[] a = {53,61,49,67,55,63};
+		int[] a = {9,2,5,4,12,7,8,11,9,3,7,4,12,5,4,10,9,6,9};
 
-		// System.out.println(findMax(a));
-		// System.out.println(findMin(a));
-		// System.out.println(findMean(a));
-		// System.out.println(findMedian(a));
-		// System.out.println(findQ1(a));
-		// System.out.println(findQ3(a));
-		// System.out.println(findMode(a));
-		System.out.println(findStandardDeviation(a));
+		// System.out.println(max(a));
+		// System.out.println(min(a));
+		// System.out.println(mean(a));
+		// System.out.println(median(a));
+		// System.out.println(quartile1(a));
+		// System.out.println(quartile3(a));
+		// System.out.println(mode(a));
+		System.out.println(standardDeviation(a));
 
 	}
 
 
-	public static int findMax(int[] a) {
+	public static int max(int[] a) {
 		int max = a[0]; 
 		for (int i=0; i<a.length; i++) {
 			if (a[i]>max) {
 				max = a[i];
 			}
-	
+
 		}
 		return max;
 	}
 
-	public static int findMin(int[] a) {
+	public static int min(int[] a) {
 		int min = a[0];
-	
+
 	for (int i=0; i<a.length; i++) {
 			if (a[i]<min) {
 				min=a[i];
@@ -46,8 +46,8 @@ public class Stats {
 		}
 		return min;
 	}
-	
-	public static double findMean(int[] a) {
+
+	public static double mean(int[] a) {
 		double mean =0;
 		int sum = 0;
 		for (int i=0; i<a.length; i++) {
@@ -59,7 +59,7 @@ public class Stats {
 
 	// 1,2,3,4
 
-	public static double findMedian(int[] a) {
+	public static double median(int[] a) {
 		double median = 0;
 		int mid = a.length/2;
 		if (a.length % 2 ==0) {
@@ -68,10 +68,10 @@ public class Stats {
 			median = a[mid];
 		}
 		return median;
-		
+
 	}
-	
-	public static double findQ1(int[] a) {
+
+	public static double quartile1(int[] a) {
 		double length = a.length;
 		double quarter = length/4;
 		int placement = (int)quarter;
@@ -85,7 +85,7 @@ public class Stats {
 		return number;
 	}
 
-	public static double findQ3(int[] a) {
+	public static double quartile3(int[] a) {
 		double length = a.length;
 		double quarter = length/4;
 		double multiply = quarter * 3;
@@ -100,7 +100,7 @@ public class Stats {
 		return number;
 	}
 
-	public static int findMode(int[] a) {
+	public static int mode(int[] a) {
 		int temporary, counter, number;
 		counter=0;
 		number=0;
@@ -119,8 +119,8 @@ public class Stats {
 		return number;
 	}
 
-	public static double findStandardDeviation(int[] a) {
-		double mean = findMean(a);
+	public static double standardDeviation(int[] a) {
+		double mean = mean(a);
 		double sum = 0;
 		double variance =0;
 		for (int i=0; i<a.length; i++) {
@@ -128,7 +128,7 @@ public class Stats {
 			double square = diff * diff;
 			sum += square;
 		}
-		variance = sum/a.length - 1;
+		variance = sum/(a.length-1);
 		double sd = Math.sqrt(variance);
 		return sd;
 	}
